fd2c312e43fbace329b37bec753425f41f80b0d8
mayara-canaver <mayaracanaver@hotmail.com>
2022-01-07 09:47:38 -0300

    update code with some automations

--- main.py
+++ main.py
@@ -1,47 +1,46 @@
 import pandas as pd
 import numpy as np
-import pandas_gbq
 
 from google_play_scraper import reviews_all
 from google.oauth2 import service_account
-from google.cloud import bigquery
-from pandas.io import gbq
 
 pd.options.display.max_columns = None
 
-alexa_resultado = reviews_all(
-    "com.amazon.dee.app",
+endereco = str(input("Entre com o endereco do app:\n"))
+
+app = reviews_all(
+    endereco,
     lang="pt",
     country="br"
 )
 
-alexa_df = pd.DataFrame.from_dict(alexa_resultado)
+app_df = pd.DataFrame.from_dict(app)
 
-alexa_df = alexa_df.drop(["reviewId", "userName", "userImage", "replyContent", "repliedAt"], axis=1)
+app_df = app_df.drop(["reviewId", "userName", "userImage", "replyContent", "repliedAt"], axis=1)
 
-alexa_df.columns = [x.upper() for x in alexa_df.columns]
+app_df.columns = [x.upper() for x in app_df.columns]
 
-alexa_df["CONTENT"] = alexa_df["CONTENT"].str.strip()
-alexa_df["CONTENT"] = alexa_df["CONTENT"].str.upper()
-alexa_df["CONTENT"] = alexa_df["CONTENT"].replace('"', '\"')
+app_df["CONTENT"] = app_df["CONTENT"].str.strip()
+app_df["CONTENT"] = app_df["CONTENT"].str.upper()
+app_df["CONTENT"] = app_df["CONTENT"].replace('"', '\"')
 
-cols = alexa_df.select_dtypes(include=[np.object]).columns
-alexa_df[cols] = alexa_df[cols].apply(lambda x: x.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
+cols = app_df.select_dtypes(include=[np.object]).columns
+app_df[cols] = app_df[cols].apply(lambda x: x.str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8'))
 
-alexa_df = alexa_df.dropna()
+app_df = app_df.dropna()
 
-positivo = alexa_df.loc[alexa_df["SCORE"] >= 4]
+positivo = app_df.loc[app_df["SCORE"] >= 4]
 positivo = positivo[:-10]
 
-neutro = alexa_df.loc[alexa_df["SCORE"] == 3]
+neutro = app_df.loc[app_df["SCORE"] == 3]
 neutro = neutro[:-10]
 
-negativo = alexa_df.loc[alexa_df["SCORE"] < 3]
+negativo = app_df.loc[app_df["SCORE"] < 3]
 negativo = negativo[:-10]
 
-positivo.to_csv(r"C:/Users/Mayara Lopes/Desktop/sauter/positivo.csv", index=False)
-neutro.to_csv(r"C:/Users/Mayara Lopes/Desktop/sauter/neutro.csv", index=False)
-negativo.to_csv(r"C:/Users/Mayara Lopes/Desktop/sauter/negativo.csv", index=False)
+positivo.to_csv(r"/Desktop/positivo.csv", index=False)
+neutro.to_csv(r"/Desktop/neutro.csv", index=False)
+negativo.to_csv(r"/Desktop/negativo.csv", index=False)
 
 key_path = "GBQ.json"
 
